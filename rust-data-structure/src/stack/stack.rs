a8101f01829bf21600b3904ad8013ad02527f8cc
akatsuke <8422126+akatsukee@user.noreply.gitee.com>
2023-08-07 23:19:57 +0800

    first commit

---
+++ rust-data-structure/src/stack/stack.rs
@@ -0,0 +1,88 @@
+#[#[derive(Debug)]]
+struct Stack<T> {
+    size: usize, // 栈大小
+    data: Vec<T>, // 栈内数据
+}
+
+impl<T> Stack<T> {
+    // 初始化空栈
+    fn new() -> Self {
+        Self {
+            size: 0,
+            data: Vec::new(),
+        }
+    }
+
+    fn is_empty(&self) -> bool {
+        self.size == 0
+    }
+
+    fn len(&self) -> usize {
+        self.size
+    }
+
+    // 清空栈
+    fn clear(&self){
+        self.size = 0;
+        self.data.clear();
+    }
+
+    // 插入数据
+    fn push(&mut self, val: T) {
+        self.data.push(val);
+        self.size ++;
+    }
+
+    // 弹出栈顶元素
+    fn pop(&mut self) -> Option<T> {
+        if is_empty() {
+            return None;
+        }
+
+        self.size --;
+        sele.data.pop()
+    }
+
+    // 返回栈顶元素和可变引用
+    fn peek(&self) -> Option<&T> {
+        if is_empty() {
+            return None;
+        }
+
+        self.data.get(self.size - 1)
+    }
+
+
+    fn peek_mut(&mut self) -> Option<&mut T> {
+        if is_empty() {
+            return None;
+        }
+
+        self.data.get_mut(self.size - 1)
+    }
+
+}
+
+// 迭代器1： 栈改变，成为迭代器
+struct IntoIter<T>(Stack<T>);
+impl<T: Clone> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.0.is_empty() {
+            self.0.size --;
+            self.0.data.pop()
+        }else {
+            None
+        }
+    }
+}
+// 迭代器2： 栈不变，得到不可变迭代器
+struct Iter<`a, T: `a>{stack:: Vec<&`a T>, }
+impl<`a, T> Iterator for Iter<`a, T> {
+    type Item = &a T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.stack.pop()
+    }
+}
+
+// 迭代器3： 栈不变，得到可变迭代器
