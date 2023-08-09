b855fa4ecd27aabde4e8fed62bd098f7a820ce06
akatsuke <8422126+akatsukee@user.noreply.gitee.com>
2023-08-09 23:40:42 +0800

    add: stack unit test

--- rust-data-structure/src/main.rs
+++ rust-data-structure/src/main.rs
@@ -1,3 +1,14 @@
+mod stack;
+use stack::stack::Stack;
 fn main() {
     println!("Hello, world!");
+}
+
+#[test]
+fn test_stack() {
+    let mut  s = Stack::new();
+    s.push(1);
+    s.push(1);
+    s.push(1);
+    assert_eq!(s.len(), 3);
 }
--- rust-data-structure/src/stack/stack.rs
+++ rust-data-structure/src/stack/stack.rs
@@ -1,51 +1,51 @@
-#[#[derive(Debug)]]
-struct Stack<T> {
+#[derive(Debug)]
+pub struct Stack<T> {
     size: usize, // 栈大小
     data: Vec<T>, // 栈内数据
 }
 
 impl<T> Stack<T> {
     // 初始化空栈
-    fn new() -> Self {
+   pub fn new() -> Self {
         Self {
             size: 0,
             data: Vec::new(),
         }
     }
 
-    fn is_empty(&self) -> bool {
+    pub fn is_empty(&self) -> bool {
         self.size == 0
     }
 
-    fn len(&self) -> usize {
+    pub fn len(&self) -> usize {
         self.size
     }
 
     // 清空栈
-    fn clear(&self){
+    pub fn clear(&mut self){
         self.size = 0;
         self.data.clear();
     }
 
     // 插入数据
-    fn push(&mut self, val: T) {
+    pub fn push(&mut self, val: T) {
         self.data.push(val);
-        self.size ++;
+        self.size += 1;
     }
 
     // 弹出栈顶元素
-    fn pop(&mut self) -> Option<T> {
-        if is_empty() {
+    pub fn pop(&mut self) -> Option<T> {
+        if self.is_empty() {
             return None;
         }
 
-        self.size --;
-        sele.data.pop()
+        self.size -= 1;
+        self.data.pop()
     }
 
     // 返回栈顶元素和可变引用
-    fn peek(&self) -> Option<&T> {
-        if is_empty() {
+    pub fn peek(&self) -> Option<&T> {
+        if self.is_empty() {
             return None;
         }
 
@@ -53,8 +53,8 @@ impl<T> Stack<T> {
     }
 
 
-    fn peek_mut(&mut self) -> Option<&mut T> {
-        if is_empty() {
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        if self.is_empty() {
             return None;
         }
 
@@ -64,12 +64,12 @@ impl<T> Stack<T> {
 }
 
 // 迭代器1： 栈改变，成为迭代器
-struct IntoIter<T>(Stack<T>);
+pub  struct IntoIter<T>(Stack<T>);
 impl<T: Clone> Iterator for IntoIter<T> {
     type Item = T;
     fn next(&mut self) -> Option<Self::Item> {
         if self.0.is_empty() {
-            self.0.size --;
+            self.0.size -= 1;
             self.0.data.pop()
         }else {
             None
@@ -77,12 +77,19 @@ impl<T: Clone> Iterator for IntoIter<T> {
     }
 }
 // 迭代器2： 栈不变，得到不可变迭代器
-struct Iter<`a, T: `a>{stack:: Vec<&`a T>, }
-impl<`a, T> Iterator for Iter<`a, T> {
-    type Item = &a T;
+pub struct Iter<'a, T: 'a>{stack: Vec<&'a T>, }
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
     fn next(&mut self) -> Option<Self::Item> {
         self.stack.pop()
     }
 }
 
 // 迭代器3： 栈不变，得到可变迭代器
+pub struct IterMut<'a, T: 'a>{stack: Vec<&'a mut T>}
+impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.stack.pop()
+    }
+}
